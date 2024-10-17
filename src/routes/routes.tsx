17d421aa6ebedc542b0e63aaa77075f5ac3c31ce
aminul <rahathssn@gmail.com>
2024-10-18 03:42:07 +0600

    added register and login component

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/Home/Home";
+import Register from "../pages/Register/Register";
+import Login from "../pages/Login/Login";
 
 const router = createBrowserRouter([
   {
@@ -12,6 +14,14 @@ const router = createBrowserRouter([
         element: <Home />,
       },
     ],
+  },
+  {
+    path: "register",
+    element: <Register />,
+  },
+  {
+    path: "login",
+    element: <Login />,
   },
 ]);
 
