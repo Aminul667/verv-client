3f04ad71057b7524ace509a6d53b4024ae7d11db
aminul <rahathssn@gmail.com>
2024-10-18 03:37:28 +0600

    added layout and outlet

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,7 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../../pages/Shared/Navbar/Navbar";
+import Footer from "../../pages/Shared/Footer/Footer";
+
 const MainLayout = () => {
   return (
     <div>
-      <h2>This is MainLayout</h2>
+      <Navbar />
+      <Outlet />
+      <Footer />
     </div>
   );
 };
