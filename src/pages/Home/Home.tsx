a2f30cb2f6a1c858853037821cc91bb86b5d7405
aminul <rahathssn@gmail.com>
2024-10-21 21:51:22 +0600

    added @types/node for digitalocean

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -22,57 +22,6 @@ import { SubmitHandler, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { NextArrow, PreviousArrow } from "../../utils/Arrows";
 
-// carousel check
-// const PreviousArrow = ({ className, style, onClick }) => {
-//   return (
-//     <div
-//       className="z-10 left-[calc(50%-50px)] absolute -bottom-14 transform translate-x-full bg-gray-300 hover:bg-gray-400 text-gray-800 rounded-full p-2 cursor-pointer"
-//       onClick={onClick}
-//     >
-//       <svg
-//         xmlns="http://www.w3.org/2000/svg"
-//         className="h-6 w-6 text-gray-800"
-//         fill="none"
-//         viewBox="0 0 24 24"
-//         stroke="currentColor"
-//       >
-//         <path
-//           strokeLinecap="round"
-//           strokeLinejoin="round"
-//           strokeWidth={2}
-//           d="M15 19l-7-7 7-7"
-//         />
-//       </svg>
-//     </div>
-//   );
-// };
-
-// const NextArrow = ({ onClick }) => {
-//   return (
-//     <div
-//       className="absolute -bottom-14 right-[calc(50%-50px)] transform translate-x-full bg-gray-300 hover:bg-gray-400 text-gray-800 rounded-full p-2 cursor-pointer"
-//       onClick={onClick}
-//     >
-//       <svg
-//         xmlns="http://www.w3.org/2000/svg"
-//         className="h-6 w-6"
-//         fill="none"
-//         viewBox="0 0 24 24"
-//         stroke="currentColor"
-//       >
-//         <path
-//           strokeLinecap="round"
-//           strokeLinejoin="round"
-//           strokeWidth={2}
-//           d="M9 5l7 7-7 7"
-//         />
-//       </svg>
-//     </div>
-//   );
-// };
-
-// end carousel check
-
 const imageList = [ellipse2, ellipse3, ellipse4, ellipse5, ellipse6];
 
 const popularCardData: IProperty[] = [
